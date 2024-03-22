7388942bb5c828e81fa17c15910a0c36d611ba3d
Pablo <pablo91132@gmail.com>
2024-03-22 11:43:23 -0600

    simbologia

--- src/pages/registro/Registro.jsx
+++ src/pages/registro/Registro.jsx
@@ -5,9 +5,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import Container from "../../components/presentational/Container";
 import MultiStep from "react-multistep";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+import "./style.css";
+import imagenFondo from "../../assets/img/fondo.jpeg"
+import imgLesionCariosa from "../../assets/img/simbologia/LesionCariosa.png"
+import imgIndExt from "../../assets/img/simbologia/Indicadoparaextraerse.png"
+import imgObturado from "../../assets/img/simbologia/Obturado.png"
+import imgEspacioVacio from "../../assets/img/simbologia/EspacioVacio.png"
+import imgDiSupNum from "../../assets/img/simbologia/DienteSuperiorNumerado.png"
+import imgRetGin from "../../assets/img/simbologia/RetraccionGingival.png"
+import imgMovDent from "../../assets/img/simbologia/MovilidadDentaria.png"
+
 
 const steps = [
-  { component: <StepOne /> },
+  { title: "Datos personales", component: <StepOne /> },
   { component: <StepTwo /> },
   { component: <StepThree /> },
   { component: <StepFour /> },
@@ -15,6 +26,7 @@ const steps = [
   { component: <StepSix /> },
   { component: <StepSeven /> },
   { component: <StepEight /> },
+  { component: <StepNine /> },
 ];
 
 function StepOne() {
@@ -647,6 +659,172 @@ function StepEight() {
   );
 }
 
+function StepNine() {
+  const [identificador, setIdentificador] = useState("");
+
+  const [pointsLC, setPointsLC] = useState([]);
+  const [pointsIE, setPointsIE] = useState([]);
+  const [pointsOB, setPointsOB] = useState([]);
+  const [pointsEV, setPointsEV] = useState([]);
+  const [pointsDSN, setPointsDSN] = useState([]);
+  const [pointsRG, setPointsRG] = useState([]);
+  const [pointsMD, setPointsMD] = useState([]);
+
+  const handleClick = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    if (identificador === "LC") {
+      setPointsLC([...pointsLC, { x, y }]);
+    } else if (identificador === "IE") {
+      setPointsIE([...pointsIE, { x, y }]);
+    } else if (identificador === "OB") {
+      setPointsOB([...pointsOB, { x, y }]);
+    } else if (identificador === "EV") {
+      setPointsEV([...pointsEV, { x, y }]);
+    } else if (identificador === "DSN") {
+      setPointsDSN([...pointsDSN, { x, y }]);
+    } else if (identificador === "RG") {
+      setPointsRG([...pointsRG, { x, y }]);
+    } else if (identificador === "MD") {
+      setPointsMD([...pointsMD, { x, y }]);
+    }
+  };
+
+  return (
+    <div>
+      <div className="btnsSimbologia">
+        <Button onClick={() => setIdentificador("LC")} variant="dark">Lesion cariosa</Button>
+        <Button onClick={() => setIdentificador("IE")} variant="dark">
+          Indicado para extraerse
+        </Button>
+        <Button onClick={() => setIdentificador("OB")} variant="dark">Obturado</Button>
+        <Button onClick={() => setIdentificador("EV")} variant="dark">Espacio vacio</Button>
+        <Button onClick={() => setIdentificador("DSN")} variant="dark">
+          Diente superior numerado
+        </Button>
+        <Button onClick={() => setIdentificador("RG")} variant="dark">
+          Retracción gingival
+        </Button>
+        <Button onClick={() => setIdentificador("MD")} variant="dark">
+          Movilidad dentaria
+        </Button>
+      </div>
+      <div className="divFig">
+        <div className="divImageFig">
+          <img src={imagenFondo} className="imagenFondoFig"/>
+        </div>
+        <div className="divFigCa">
+          <div
+            style={{
+              width: "82vw",
+              height: "40vh",
+              margin: "auto",
+            }}
+            onClick={handleClick}
+          >
+            {pointsLC.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              >
+                <img src={imgLesionCariosa} width="100%" height="100%" />
+              </div>
+            ))}
+            {pointsIE.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              >
+                <img src={imgIndExt} width="100%" height="100%" />
+              </div>
+            ))}
+            {pointsOB.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              ><img src={imgObturado} width="100%" height="100%" /></div>
+            ))}
+            {pointsEV.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              ><img src={imgEspacioVacio} width="100%" height="100%" /></div>
+            ))}
+            {pointsDSN.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              ><img src={imgDiSupNum} width="100%" height="100%" /></div>
+            ))}
+            {pointsRG.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              ><img src={imgRetGin} width="100%" height="100%" /></div>
+            ))}
+            {pointsMD.map((point, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  left: `${point.x}px`,
+                  top: `${point.y}px`,
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                }}
+              ><img src={imgMovDent} width="100%" height="100%" /></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const Registro = () => {
   return (
     <Autenticate>
@@ -675,7 +853,32 @@ const Registro = () => {
                 <br />
               </div>
             </div>
-            <MultiStep steps={steps} />
+            <div >
+              <MultiStep
+                steps={steps}
+                prevButton={{
+                  title: "Atras",
+                  style: {
+                    background: "#fff3cd",
+                    borderColor: "#ffeeba",
+                    color: "#856404",
+                    borderRadius: "5px",
+                    padding: "10px 20px",
+                  },
+                }}
+                nextButton={{
+                  title: "Siguente",
+                  style: {
+                    background: "#d1ecf1",
+                    borderColor: "#bee5eb",
+                    color: "#007bff",
+                    marginLeft: "50%",
+                    borderRadius: "5px",
+                    padding: "10px 20px",
+                  },
+                }}
+              />
+            </div>
           </div>
         </div>
       </Container>
