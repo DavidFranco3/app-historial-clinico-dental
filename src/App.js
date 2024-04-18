3f189290ad74af2c909fed132a51e64671214d25
DavidFranco3 <josedavidayalafranco3@gmail.com>
2024-04-17 18:37:58 -0600

    corrrecciones

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import './App.css';
 import Routing from "./routers/Routing";
+import Login from './pages/registro/login';
 import { AuthContext } from "./utils/contexts";
+import { isUserLogedApi } from "./api/auth";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [LoadUser, setLoadUser] = useState(false);
-  const [refreshCheckLogin, setRefreshCheckLogin] = useState(false);
+    const [user, setUser] = useState(null);
+    const [LoadUser, setLoadUser] = useState(false);
+    const [refreshCheckLogin, setRefreshCheckLogin] = useState(false);
 
-  useEffect(() => {
-    // You can perform any initial setup here if needed
-    setRefreshCheckLogin(false);
-    setLoadUser(true);
-  }, []);
+    useEffect(() => {
+        setUser(isUserLogedApi());
+        setRefreshCheckLogin(false);
+        setLoadUser(true);
+    }, [refreshCheckLogin]);
 
-  return (
-    <AuthContext.Provider value={null}>
-      <Routing setRefreshCheckLogin={setRefreshCheckLogin} />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnVisibilityChange
-        draggable
-        pauseOnHover
-      />
-    </AuthContext.Provider>
-  );
+    if (!LoadUser) return null;
+
+    return (
+        <AuthContext.Provider value={user}>
+            {user ?
+                (
+                    <>
+                        <Routing
+                            setRefreshCheckLogin={setRefreshCheckLogin}
+                        />
+                    </>
+                )
+                :
+                (
+                    <Login
+                        setRefreshCheckLogin={setRefreshCheckLogin}
+                    />
+                )
+            }
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnVisibilityChange
+                draggable
+                pauseOnHover
+            />
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
--- src/routers/configRouting.js
+++ src/routers/configRouting.js
@@ -1,10 +1,15 @@
 import Registro from "../pages/registro/Registro";
 import TablaRegistros from "../pages/registro/TablaRegistros";
+import Login from "../pages/registro/login";
 
 export default [
   {
     path: "/registro",
     page: Registro,
+  },
+  {
+    path: "/login",
+    page: Login,
   },
   {
     path: "/tablaRegistros",
