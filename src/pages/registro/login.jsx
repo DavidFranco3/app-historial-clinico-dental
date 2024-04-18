3345d9f63778615a41f030b9934e6df058fbd4b2
Pablo <pablo91132@gmail.com>
2024-04-18 15:51:57 -0600

    diseño login

--- src/pages/registro/login.jsx
+++ src/pages/registro/login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { login, setTokenApi } from "../../api/auth";
 import { toast } from "react-toastify";
 import { jwtDecode } from "jwt-decode";
@@ -6,142 +6,138 @@ import { Spinner, Button, Form, Image, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { obtenerUsuario } from "../../api/usuarios";
+import "./styleLogin.css";
+import imagenLogin from "../../assets/img/rehabilitacionbucal.png";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons/faRightToBracket";
 
 function Login({ setRefreshCheckLogin }) {
-  const [formData, setFormData] = useState(initialFormValue)
-  const [signInLoading, setSignInLoading] = useState(false)
+  const [formData, setFormData] = useState(initialFormValue);
+  const [signInLoading, setSignInLoading] = useState(false);
 
-  const [mostrarPassword, setMostrarPassword] = useState(false)
+  const [mostrarPassword, setMostrarPassword] = useState(false);
   const togglePasswordVisiblity = () => {
-    setMostrarPassword((val) => !val)
-  }
+    setMostrarPassword((val) => !val);
+  };
 
   const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (!formData.password) {
-      toast.warning('Completa todos los campos del formulario.')
+      toast.warning("Completa todos los campos del formulario.");
     } else {
-      setSignInLoading(true)
+      setSignInLoading(true);
       try {
         login(formData)
           .then((response) => {
             const {
               data: { token },
-            } = response
-            setTokenApi(token)
-            const { _ } = jwtDecode(token)
-            const idUdsuario = _
+            } = response;
+            setTokenApi(token);
+            const { _ } = jwtDecode(token);
+            const idUdsuario = _;
             try {
-              obtenerUsuario(idUdsuario).then(response => {
+              obtenerUsuario(idUdsuario).then((response) => {
                 const { data } = response;
-                toast.success('Bienvenido ' + data.nombre)
-                setRefreshCheckLogin(true)
-              })
+                toast.success("Bienvenido " + data.nombre);
+                setRefreshCheckLogin(true);
+              });
               //enrutamiento("/Dashboard");
             } catch (ex) {
-              toast.error('Error al obtener el usuario')
+              toast.error("Error al obtener el usuario");
             }
           })
           .catch((ex) => {
-            if (ex.message === 'Network Error') {
-              toast.error('Conexión al servidor no disponible')
-              setSignInLoading(false)
+            if (ex.message === "Network Error") {
+              toast.error("Conexión al servidor no disponible");
+              setSignInLoading(false);
             } else {
               if (ex.response && ex.response.status === 401) {
-                const { mensaje } = ex.response.data
-                toast.error(mensaje)
-                setSignInLoading(false)
+                const { mensaje } = ex.response.data;
+                toast.error(mensaje);
+                setSignInLoading(false);
               }
             }
-          })
+          });
       } catch (ex) {
-        toast.error('Error al iniciar sesión')
-        setSignInLoading(false)
+        toast.error("Error al iniciar sesión");
+        setSignInLoading(false);
       }
     }
-  }
+  };
 
   const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
-    <section className="h-screen">
-      <div className="container px-6 py-12 h-full">
-        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-          <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0 space-y-4">
-            <div className="logos">
-              {/*<Image
-                className="logoPrincipal1"
-                src={"LogoLANENA"}
-                alt="Tortas la Nena"
-                title="Tortas la Nena"
-  />*/}
+    <div className="princContainer">
+      <div className="container-princ">
+        <div className="form-container">
+          <div className="mb-4">
+            <img src={imagenLogin} width="100%" height="100%" />
+          </div>
+          <Form onSubmit={onSubmit} onChange={onChange}>
+            <div>
+              <Form.Control
+                type={mostrarPassword ? "text" : "password"}
+                name="password"
+                defaultValue={formData.password}
+                placeholder="Contraseña"
+                className="txtPassword"
+              />
+              <FontAwesomeIcon
+                title="Mostrar contraseña"
+                icon={!mostrarPassword ? faEyeSlash : faEye}
+                onClick={togglePasswordVisiblity}
+              />{" "}
+              Mostrar contraseña
             </div>
-            <Form onSubmit={onSubmit} onChange={onChange}>
-              <div className="flex items-center mb-6">
-                <Form.Control
-                  type={mostrarPassword ? 'text' : 'password'}
-                  name="password"
-                  defaultValue={formData.password}
-                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  placeholder="Contraseña"
-                />
-                <FontAwesomeIcon
-                  title="Mostrar contraseña"
-                  className="cursor-pointer py-2 -ml-6"
-                  icon={!mostrarPassword ? faEyeSlash : faEye}
-                  onClick={togglePasswordVisiblity}
-                />
-              </div>
-              <div className="pt-6">
-                <Button
-                  title="Iniciar sesión"
-                  type="submit"
-                  className="inline-block px-7 py-3 bg-blue-600 text-white font-bold text-lg leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                  data-mdb-ripple="true"
-                  variant="primary"
-                  disabled={signInLoading}
-                >
-                  {!signInLoading ? (
-                    'Iniciar Sesión'
-                  ) : (
-                    <Spinner animation="border" />
-                  )}
-                </Button>
-              </div>
-            </Form>
-          </div>
-
-        </div>
-        <div className="w-full text-center lg:text-left">
-          <div className="text-gray-700 text-center p-4">
-            © {
-              // Get current year
-              new Date().getFullYear()
-            } Copyright:{' '}
-            <a
-              className="pie text-emerald-700 no-underline"
-              href="https://ideasysolucionestecnologicas.com"
-              target="_blank"
-              rel="noreferrer"
-              title="Ir al sitio web de la empresa"
-            >
-              Ideas y Soluciones Tecnológicas S.A de C.V
-            </a>
-          </div>
+            <div className="d-flex justify-content-center mt-3">
+              <Button
+                title="Iniciar sesión"
+                type="submit"
+                data-mdb-ripple="true"
+                className="button_slide slide_right"
+                disabled={signInLoading}
+              >
+                {!signInLoading ? (
+                  <>
+                    <FontAwesomeIcon icon={faRightToBracket} /> Iniciar Sesión
+                  </>
+                ) : (
+                  <Spinner animation="border" />
+                )}
+              </Button>
+            </div>
+          </Form>
         </div>
       </div>
-    </section>
-  )
+      <div class="screen__background">
+        <span class="screen__background__shape screen__background__shape4"></span>
+        <span class="screen__background__shape screen__background__shape2"></span>
+      </div>
+      <footer className="footer">
+        <div>
+          ©{new Date().getFullYear()} Copyright:{" "}
+          <a
+            href="https://ideasysolucionestecnologicas.com"
+            target="_blank"
+            rel="noreferrer"
+            title="Ir al sitio web de la empresa"
+          >
+            Ideas y Soluciones Tecnológicas S.A de C.V
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 function initialFormValue() {
   return {
-    usuario: '',
-    password: '',
-  }
+    usuario: "",
+    password: "",
+  };
 }
 
 export default Login;
